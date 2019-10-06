d7ed68d016989db388a2114f70c0093831ee0bf5
kj <kristoferjoseph@users.noreply.github.com>
2019-10-06 18:55:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{message}</p>
-        <p>Change me!</p>
+        <p>Changed!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
